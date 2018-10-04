01043ec4cb98dc46c6ed7d9ef2a98cd0b63eba4d
fiorellaeffio <fiorella.effio@unmsm.edu.pe>
2018-10-04 11:53:53 -0500

    login admi

---
+++ src/js/writeData.js
@@ -0,0 +1,15 @@
+document.getElementById('loginUser').addEventListener('click', () => {
+  let email = document.getElementById('emailUser').value;
+  let password = document.getElementById('passwordUser').value;
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
+    document.getElementById('messageError').innerHTML = "";
+    let user = firebase.auth().currentUser;
+    console.log(user.uid);
+  })
+  .catch(function (error) {
+    var errorMessage = error.message;
+    document.getElementById('messageError').style.color = 'red';
+    document.getElementById('messageError').innerHTML = "Usuario no existe o la contraseña no coincide.";
+    console.log;
+  });
+});
